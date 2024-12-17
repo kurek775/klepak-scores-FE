48f46f0ec60fc00677031594af374b21f22a61f6
kurek775 <pavelkurek@seznam.cz>
2024-12-17 17:38:44 +0100

    WIP - view uploaded file

--- client/src/app/api/services/upload.service.ts
+++ client/src/app/api/services/upload.service.ts
@@ -1,29 +1,24 @@
 export class UploadService {
-  private readonly apiBaseUrl = "http://127.0.0.1:8000/api";
+  private readonly apiBaseUrl =  "http://127.0.0.1:8000/api";
 
-  async uploadBlob(blob: Blob, tourId: number, crewId: number) {
+  async uploadBlob(blob: Blob, tourId: number, crewId: number):Promise<any> {
     const formData = new FormData();
+    formData.append("file", blob, "example-file.jpg");
 
-    // Přidání Blobu jako souboru do FormData
-    formData.append("file", blob, "example-file.jpg"); // "example-file.jpg" je název souboru, který bude zobrazen na backendu
-
-    try {
-      const response = await fetch(
-        `${this.apiBaseUrl}/tours/${tourId}/crews/${crewId}/upload-photo`,
-        {
-          method: "POST",
-          body: formData,
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to upload file");
+    const response = await fetch(
+      `${this.apiBaseUrl}/tours/${tourId}/crews/${crewId}/upload-photo`,
+      {
+        method: "POST",
+        body: formData,
+        cache: "no-store",
       }
+    );
 
-      const result = await response.json();
-      console.log(result.message);
-    } catch (error) {
-      console.error("Error uploading file:", error);
+    if (!response.ok) {
+      throw new Error("Failed to upload file");
     }
+
+    const res = await response.json();
+    return res.message.sport as any;
   }
 }
--- client/src/app/components/atoms/file-uploader.tsx
+++ client/src/app/components/atoms/file-uploader.tsx
@@ -4,14 +4,16 @@ import React, { useState } from "react";
 
 const FileUploader: React.FC = () => {
   const [fileName, setFileName] = useState<string | null>(null);
+  const [res,setResp]=useState<any | null>(null);
   const service = new UploadService();
-  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
 
     if (file) {
       setFileName(file.name);
       console.log("Nahraný soubor:", file);
-      service.uploadBlob(file as Blob, 1, 1);
+      const r = await service.uploadBlob(file as Blob, 1, 1);
+      setResp(r);
     }
   };
 
@@ -20,6 +22,7 @@ const FileUploader: React.FC = () => {
       style={{ padding: "20px", border: "1px solid #ccc", borderRadius: "5px" }}
     >
       <h3>Nahrát soubor</h3>
+      <div>{res}</div>
       <input type="file" onChange={handleFileUpload} />
       {fileName && (
         <p>
