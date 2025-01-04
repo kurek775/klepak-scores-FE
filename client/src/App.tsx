a3cb49460a080d862d5f474ce933aa0fe6c94d90
kurek775 <pavelkurek@seznam.cz>
2025-01-04 10:26:58 +0100

    DONE - basic routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,68 +1,16 @@
-import { useState } from "react";
 import "./App.css";
-import Table from "pk-editable-table-component";
-import { FileUploader } from "./components/fileUploader";
-
-import { UploadResponse, Record as Rec } from "./api/types/response";
+import TestPage from './pages/Test';
+import { Route, Routes } from "react-router-dom"
 function App() {
-  const headers = [
-    {
-      columnLabel: "Jméno",
-      key: "name",
-      type: "text",
-      disabled: true,
-      required: true,
-      filterDisabled: true,
-      sorterDisabled: true,
-    },
-    {
-      columnLabel: "Výsledek",
-      key: "score",
-      type: "number",
-      required: true,
-      filterDisabled: true,
-      sorterDisabled: true,
-    },
-  ];
-  const [editable, setEditable] = useState<boolean>(false);
-
-  const [data, setData] = useState<Rec[]>([]);
-
-  const handleUploadedResponse = (r: UploadResponse<Rec>) => {
-    setData(r.list);
-  };
-
-  const handleSubmit = (data: any) => {
-    console.log(data);
-  }
-
   return (
-    <>
-      <div>
-        <FileUploader onUploadComplete={handleUploadedResponse} />
-        {!editable && Boolean(data.length)  && <button
-          className="control-button"
-          onClick={() => setEditable(true)}
-        >
-          Editovat
-        </button>}
-        {Boolean(data.length) && <Table
-          keyVal="id"
-          initialData={data}
-          onSubmit={handleSubmit}
-          headers={headers}
-          editable={editable}
-          text={{ delete: "SMAZAT", addRow: "PŘIDAT", submit: "ULOŽIT" }}
-          actions={{
-            create: false,
-            edit: true,
-            delete: false,
-          }}
-        />}
-    
+   <>
+  
+      <div className="App">
+      <Routes>
+          <Route path="/" element={<TestPage />} />
+        </Routes>
       </div>
     </>
-  );
+ );
 }
-
-export default App;
+export default App
